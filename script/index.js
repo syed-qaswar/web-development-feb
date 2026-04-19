31a5e15fb2cd4a7334c8cf644abb85b705d2c6de
Syed Qaswar <qaswarbukhari5@gmail.com>
2026-04-19 15:45:12 +0500

    tailwinds and js tasks

---
+++ script/index.js
@@ -0,0 +1,44 @@
+// variables: a named memory location
+// data types
+let name = 'Ali'; //strings
+let age = 20 //integer
+let city = 'Lahore'
+let marks = 78.9 //float
+let pass_status = true; //boolean
+const roll_num = '1A'
+// const roll_num = '1B'
+
+// output
+console.log(name);
+console.log(age);
+console.log(city);
+
+// Operators
+let num1 = 10
+let num2 = 3
+
+// arithmetic Operators
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+console.log(num1 % num2)
+
+// assigment operators
+// num1 = num1 + 2
+
+num1 += 2 //12
+num1 -= 2 //10
+num1 *= 3 //30
+
+// comparison operators
+console.log(num1 > num2)
+console.log(num1 < num2)
+
+// logical operators
+console.log(2 == 2 && 2 > 4)
+console.log(2 == 2 || 2 > 4)
+
+
+// w3schools, programiz, geeksforgeeks
+
